9185059738e82ca795439178e4e7c92364e73509
lefarov <mlefarov@gmail.com>
2020-09-29 18:28:36 +0200

    Writing transitions to the buffer is working.

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -1,5 +1,6 @@
 # %% Imports
 import time
+import pickle
 import torch
 import torch.nn as nn
 import torch.functional as F
@@ -35,13 +36,97 @@ def ndarray_to_model_state(model_array, model_state):
     return loaded_state
 
 
-def states_equal(state1, state2):
+def model_states_equal(state1, state2):
     equal = True
     for (k1, t1), (k2, t2) in zip(state1.items(), state2.items()):
         equal = equal and torch.equal(t1, t2)
         equal = equal and k1 == k2
 
     return equal
+
+
+def transition_buffer_to_bytes(buffer):
+    return b"".join(list(map(lambda t: pickle.dumps(t), buffer)))
+
+
+def bytes_to_transition_buffer(bts, transition_bsize):
+    transitions = []
+    for idx in range(0, len(bts), transition_bsize):
+        transitions.append(pickle.loads(bts[idx : idx + transition_bsize]))
+
+    return transitions
+
+
+def tranition_buffers_equal(buffer1, buffer2):
+    equal = True
+    for t1, t2 in zip(buffer1, buffer2):
+        for v1, v2 in zip(t1, t2):
+            if isinstance(v1, np.ndarray):
+                equal = equal and np.array_equal(v1, v2)
+            else:
+                equal = equal and v1 == v2
+
+    return equal
+
+
+# %% Testing functions
+def share_model(window, net):
+    # Write model state to the Learner window
+    window.Lock(rank=0)
+    window.Put(model_state_to_ndarray(net.state_dict()), target_rank=0)
+    window.Unlock(rank=0)
+
+
+def read_model(window, net):
+    # Create local copy of the network and the weights buffer
+    net_local = Net()
+    state_array = np.zeros(
+        model_state_nelements(net_local.state_dict()), dtype=np.float32
+    )
+
+    # Wait for 2 seconds
+    time.sleep(1)
+
+    # Read model state from the Learner's window
+    window.Lock(rank=0)
+    window.Get(state_array, target_rank=0)
+    window.Unlock(rank=0)
+
+    # Verify the correct weights
+    loaded_state = ndarray_to_model_state(state_array, net_local.state_dict())
+    print(
+        f"worker: {comm.Get_rank()}/{comm.Get_size()}, "
+        f"weights are identical: {model_states_equal(net.state_dict(), loaded_state)}"
+    )
+
+
+def write_transitions(window, trans):
+    # Pickle transitions
+    trans_bts = transition_buffer_to_bytes(trans)
+
+    # Write transition bytes to the Learner window
+    window.Lock(rank=0)
+    window.Put(trans_bts, target_rank=0, target=(0, len(trans_bts), MPI.BYTE))
+    window.Unlock(rank=0)
+
+
+def read_transitions(window, trans, transition_bsize):
+    # Wait for 2 seconds
+    time.sleep(2)
+
+    # Read transitions from the buffer
+    trans_window.Lock(rank=0)
+    buffer = bytearray(trans_window)
+    trans_window.Unlock(rank=0)
+
+    # Unpickle received transtions
+    trans_restored = bytes_to_transition_buffer(buffer, transition_bsize)
+
+    # Verify unpickled transtions
+    print(
+        f"worker: {comm.Get_rank()}/{comm.Get_size()}, "
+        f"transition are identical: {tranition_buffers_equal(trans_restored, trans)}"
+    )
 
 
 # %% Network
@@ -69,23 +154,38 @@ class Net(nn.Module):
         return x
 
 
-# %%
+# %% Worker and learner classes
 class Worker(object):
-    def __init__(self, mpi_comm, buffer_size, buffer_state, policy_state, n_steps=100):
+    def __init__(self, mpi_comm, buffer_size, model, model_window, n_steps=100):
         self.comm = mpi_comm
-        self.size = buffer_size
+
+        # Experiance buffer
         self.buffer = []
-        self.policy = policy_state
+        self.size = buffer_size
 
         self.step_n = n_steps
         self.step_i = 0
 
-        # Receive policy (blocking)
-        self.policy = self.comm.bcast(self.policy, root=0)
-        print(
-            f"worker: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
-            f"policy: {self.policy['W']}, "
+        # Model object and RMA window for model parameters
+        self.model = model
+        self.model_window = model_window
+
+        # Model state dictionary and parameters buffer
+        self.model_state = model.state_dict()
+        self.model_array = np.zeros(
+            model_state_nelements(self.model_state), dtype=np.float32
         )
+
+        # Read initial model parameters from the Learner's RMA window
+        self.model_window.Lock(rank=0)
+        self.model_window.Get(self.model_array, target_rank=0)
+        self.model_window.Unlock(rank=0)
+
+        # Load initial parameters to the model
+        self.model.load_state_dict(
+            ndarray_to_model_state(self.model_array, self.model_state)
+        )
+
         # Initialize empty request for sending rollouts
         self.req = MPI.Request()
 
@@ -126,7 +226,7 @@ class Worker(object):
                 self.comm.gather(self.buffer, root=0)
 
                 # Get the updated policy
-                self.policy = self.comm.bcast(self.policy, root=0)
+                self.model = self.comm.bcast(self.model, root=0)
                 print(
                     f"worker: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
                     f"policy: {self.policy['W']}"
@@ -196,12 +296,14 @@ class Learner(object):
 
 
 # %% Main
+_TEST_SIGNLE_TRANSFER = True
+
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
 net = Net()
-policy_window = MPI.Win.Allocate(model_state_bsize(net.state_dict()), comm=comm)
+model_window = MPI.Win.Allocate(model_state_bsize(net.state_dict()), comm=comm)
 
 # Assign zeros to the params of shared network
 zero_state = net.state_dict()
@@ -210,37 +312,38 @@ for key, value in zero_state.items():
 
 net.load_state_dict(zero_state)
 
+# Create array of transitions
+trans = [
+    Transition(
+        np.ones(10) * i,
+        f"s{i}",
+        0.7,
+        0.9,
+        np.ones(2),
+    )
+    for i in range(2)
+]
+
+trans_bsize = len(pickle.dumps(trans[0]))
+trans_window = MPI.Win.Allocate(trans_bsize * 2, comm=comm)
+
 if rank != 0:
-    # Create local copy of the network and the weights buffer
-    net_local = Net()
-    state_array = np.zeros(
-        model_state_nelements(net_local.state_dict()), dtype=np.float32
-    )
+    if _TEST_SIGNLE_TRANSFER:
+        read_model(model_window, net)
+        write_transitions(trans_window, trans)
 
-    # Wait for 2 seconds
-    time.sleep(2)
-
-    # Read model state from the Learner's window
-    policy_window.Lock(rank=0)
-    policy_window.Get(state_array, target_rank=0)
-    policy_window.Unlock(rank=0)
-
-    # Verify the correct weights
-    loaded_state = ndarray_to_model_state(state_array, net_local.state_dict())
-    print(
-        f"worker: {comm.Get_rank()}/{comm.Get_size()}, "
-        f"weights are identical: {states_equal(net.state_dict(), loaded_state)}"
-    )
-
+    else:
+        pass
     # worker = Worker(comm, 100, _BUFFER_GTHR_STATE, _POLICY_BCST_STATE)
     # worker.run()
 
 if rank == 0:
-    # Write global model state to the Learner window
-    policy_window.Lock(rank=0)
-    policy_window.Put(model_state_to_ndarray(net.state_dict()), target_rank=0)
-    policy_window.Unlock(rank=0)
+    if _TEST_SIGNLE_TRANSFER:
+        share_model(model_window, net)
+        read_transitions(trans_window, trans, trans_bsize)
 
+    else:
+        pass
     # learner = Learner(comm, 10000, _BUFFER_GTHR_STATE, _POLICY_BCST_STATE)
     # learner.learn()
 
