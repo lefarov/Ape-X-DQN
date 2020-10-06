8f3192e3f3a671ae98c8accf5758e73759938ef1
lefarov <mlefarov@gmail.com>
2020-10-06 21:44:08 +0200

    RMA Learner-Worker communication is working.

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -1,5 +1,6 @@
 # %% Imports
 import time
+import random
 import pickle
 import torch
 import torch.nn as nn
@@ -159,20 +160,35 @@ class Worker(object):
     def __init__(
         self,
         mpi_comm,
-        buffer_size,
         model,
         model_window,
+        transition_bsize,
         transitions_window,
-        n_steps=100,
+        transitions_idx_window,
+        transitions_send_t=200,
+        model_update_t=200,
+        local_replay_size=200,
+        total_replay_size=500,
+        total_time_steps=1000,
     ):
         self.comm = mpi_comm
 
         # Local experiance buffer
         self.buffer = []
-        self.size = buffer_size
+        self.size = local_replay_size
+        self.total_reaplay_size = total_replay_size
+        self.trans_bsize = transition_bsize
+        self.trans_send_t = transitions_send_t
+        self.model_update_t = model_update_t
+        self.total_t = total_time_steps
 
-        # RMA window for transitions
+        # RMA window for transitions and idx
         self.trans_window = transitions_window
+        self.idx_window = transitions_idx_window
+
+        # Bytearray to load the remote transition index (int32)
+        # TODO: no need to store this one
+        self.idx_barray = bytearray(4)
 
         # Model object and RMA window for model parameters
         self.model = model
@@ -185,25 +201,67 @@ class Worker(object):
         )
 
         # Read initial model parameters from the Learner's RMA window
+        self._update_model()
+
+    def _update_model(self):
+        # Lock the RMA window and read model parameters
         self.model_window.Lock(rank=0)
         self.model_window.Get(self.model_array, target_rank=0)
         self.model_window.Unlock(rank=0)
 
-        # Load initial parameters to the model
+        # Load received parameters to the model
         self.model.load_state_dict(
             ndarray_to_model_state(self.model_array, self.model_state)
         )
 
-        # Initialize empty request for sending rollouts
-        self.req = MPI.Request()
+    def _send_transitions(self):
+        # Lock the RMA window for replay buffer (we don't need lock for idx)
+        self.trans_window.Lock(rank=0)
 
-        self.step_n = n_steps
-        self.step_i = 0
+        # Read current index
+        self.idx_window.Get(self.idx_barray, target_rank=0)
+        idx = int.from_bytes(self.idx_barray, byteorder="big")
+
+        # Pickle the transitions
+        barray = transition_buffer_to_bytes(self.buffer)
+
+        # If Learner Replay has enough space
+        # TODO: move that to class properties
+        blen = self.total_reaplay_size * self.trans_bsize
+        if idx + len(barray) < blen:
+            # Write data in one block
+            self.trans_window.Put(
+                barray, target_rank=0, target=(idx, len(barray), MPI.BYTE)
+            )
+            # Increment the index
+            idx += len(barray)
+
+        else:
+            # Write data to the end of the buffer
+            n = blen % idx
+            self.trans_window.Put(
+                barray[:n],
+                target_rank=0,
+                target=(idx, n, MPI.BYTE),
+            )
+            # And to the beginning
+            self.trans_window.Put(
+                barray[n:],
+                target_rank=0,
+                target=(0, len(barray) - n, MPI.BYTE),
+            )
+            # Update the index
+            idx = len(barray) - n
+
+        # Write index back to the Learner RMA
+        self.idx_window.Put(idx.to_bytes(4, byteorder="big"), target_rank=0)
+
+        # Unlock the RMA window
+        self.trans_window.Unlock(rank=0)
 
     def run(self):
-        # TODO: collect data while previous send is not completed.
-
-        for i in range(1000000):
+        for t in range(self.total_t):
+            # Append the transition to the buffer
             self.buffer.append(
                 Transition(
                     np.random.normal(0.0, 0.5, size=(10,)),
@@ -215,99 +273,144 @@ class Worker(object):
             )
             self.buffer = self.buffer[-self.size :]
 
-            # TODO: collect n data points per policy
-            if self.step_i == self.step_n:
+            # Send data to the learner every N steps
+            if t % self.trans_send_t == 0 and t > 0:
+                self._send_transitions()
                 print(
                     f"worker: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
-                    f"iteration: {i}, t:{time.time() - start_time}, "
-                    f"data length: {len(self.buffer)}"
+                    f"iteration: {t}, t:{time.time() - start_time}, "
+                    f"has written to index: {int.from_bytes(self.idx_barray, byteorder='big')}"
                 )
 
-                # Check if previous send is completed
-                if not MPI.Request.Test(self.req):
-                    # If nto wait for completion
-                    print(
-                        f"worker: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
-                        f"previous send was not completed"
-                    )
-                    self.req.wait()
-
-                # Send rollouts to learner (non-blocking)
-                # self.req = self.comm.isend(self.buffer, dest=0, tag=11)
-                self.comm.gather(self.buffer, root=0)
-
-                # Get the updated policy
-                self.model = self.comm.bcast(self.model, root=0)
+            # Update model with the Learner weights
+            if t % self.model_update_t == 0 and t > 0:
+                self._update_model()
                 print(
                     f"worker: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
-                    f"policy: {self.policy['W']}"
+                    f"policy: {self.model.fc3.bias}"
                 )
-
-                self.step_i = 0
-
-            self.step_i += 1
 
 
 class Learner(object):
-    def __init__(self, mpi_comm, buffer_size, buffer_state, policy_state, n_steps=10):
+    def __init__(
+        self,
+        mpi_comm,
+        model,
+        model_window,
+        transition_bsize,
+        transitions_window,
+        transitions_idx_window,
+        transitions_sample_t=100,
+        model_update_t=100,
+        total_replay_size=500,
+        total_time_steps=1000,
+    ):
         self.comm = mpi_comm
-        self.size = buffer_size
-        self.buffer = []
-        self.policy = policy_state
 
-        self.step_n = n_steps
-        self.step_i = 0
+        # Experiance buffer
+        self.replay = []
+        # self.barray = bytearray(total_replay_size * trans_bsize)
 
-        # Create persistant receive communication
-        # self.req = self.communicator.Recv_init(source=1, tag=11)
+        self.size = total_replay_size
+        self.trans_bsize = transition_bsize
+        self.model_update_t = model_update_t
+        self.trans_sample_t = transitions_sample_t
+        self.total_t = total_time_steps
 
-        # Send initialized policy to all workers
-        self.policy["W"] = 0.0
-        self.comm.bcast(self.policy, root=0)
+        # RMA window for Replay buffer and idx
+        self.trans_window = transitions_window
+        self.trans_barray_idx = 0
+        self.trans_full_flag = False
+        self.idx_window = transitions_idx_window
+
+        # Initialize transtion index
+        self.trans_window.Lock(rank=0)
+        self.idx_window.Put(
+            self.trans_barray_idx.to_bytes(4, byteorder="big"), target_rank=0
+        )
+        self.trans_window.Unlock(rank=0)
+
+        # Model object and RMA window for model parameters
+        self.model = model
+        self.model_window = model_window
+
+        # Initialize model and Write model state to the Learner window
+        self.model.fc3.bais = torch.zeros_like(self.model.fc3.bias)
+        self.model_window.Lock(rank=0)
+        self.model_window.Put(
+            model_state_to_ndarray(self.model.state_dict()), target_rank=0
+        )
+        self.model_window.Unlock(rank=0)
 
     def learn(self):
-        # TODO: open policy recv request here and while not completed collect the rollouts
-        for i in range(1000):
+        for t in range(self.total_t):
+            # Send data to the learner every N steps
+            if t % self.trans_sample_t == 0 and t > 0:
+                # Lock transition RMA window
+                self.trans_window.Lock(rank=0)
 
-            if self.step_i == self.step_n:
-                print(
-                    f"learner: training iteration: {i}, t:{time.time() - start_time}, "
-                    f"data length: {len(self.buffer)}"
+                # Check if buffer was filled completely
+                # TODO: rewrite this check
+                self.trans_full_flag = self.trans_full_flag or (
+                    int.from_bytes(self.idx_window, byteorder="big")
+                    < self.trans_barray_idx
+                    and bytearray(self.trans_window)[0] != 0
                 )
 
-                # Received data from workers (non-blocking)
-                # reqs = [
-                #     self.comm.irecv(source=w, tag=11)
-                #     for w in range(1, self.comm.Get_size())
-                # ]
-
-                # Gather the data from workers (blocking)
-                data = self.comm.gather(self.buffer, root=0)
-
-                # Send updated policy to all workers (blocking)
-                self.comm.bcast(self.policy, root=0)
-
-                # Wait untill all data is received
-                # data = MPI.Request.waitall(reqs)
-
-                # Postptocess the data
                 print(
-                    f"learner: received data type: {type(data)}, len: {len(data)}, "
-                    f"my buffer len: {len(data[0])}"
+                    f"learner: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
+                    f"iteration: {t}, t:{time.time() - start_time}, "
+                    f"index: {int.from_bytes(self.idx_window, byteorder='big')}, "
+                    f"flag: {self.trans_full_flag}"
                 )
-                self.buffer.append(data)
-                self.buffer = self.buffer[-self.size :]
 
-                self.step_i = 0
+                if self.trans_full_flag:
+                    # Buffer has been filled (sample normally)
+                    trans_restored = bytes_to_transition_buffer(
+                        bytearray(self.trans_window), self.trans_bsize
+                    )
 
-            # Time deelay for training
-            time.sleep(1)
-            self.policy["W"] += 1.0
-            self.step_i += 1
+                else:
+                    # Buffer is not filled yet
+                    # Update index
+                    self.trans_barray_idx = int.from_bytes(
+                        self.idx_window, byteorder="big"
+                    )
+
+                    # Sample up to index
+                    trans_restored = bytes_to_transition_buffer(
+                        bytearray(self.trans_window)[: self.trans_barray_idx],
+                        self.trans_bsize,
+                    )
+
+                if trans_restored:
+                    trans_sampled = random.sample(trans_restored, 1)
+                    print(
+                        f"learner: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
+                        f"iteration: {t}, t:{time.time() - start_time}, "
+                        f"sampled transition: {trans_sampled[0]}"
+                    )
+
+                self.trans_window.Unlock(rank=0)
+
+            # Update model with the Learner weights
+            if t % self.model_update_t == 0 and t > 0:
+                self.model.fc3.bias = nn.Parameter(
+                    torch.ones_like(self.model.fc3.bias) * t / self.model_update_t
+                )
+                self.model_window.Lock(rank=0)
+                self.model_window.Put(
+                    model_state_to_ndarray(self.model.state_dict()), target_rank=0
+                )
+                self.model_window.Unlock(rank=0)
+                print(
+                    f"learner: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
+                    f"policy: {self.model.fc3.bias}"
+                )
 
 
 # %% Main
-_TEST_SIGNLE_TRANSFER = True
+_TEST_SIGNLE_TRANSFER = False
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
@@ -323,22 +426,35 @@ for key, value in zero_state.items():
 
 net.load_state_dict(zero_state)
 
+counter_window = MPI.Win.Allocate(4, comm=comm)
+
 # Create array of transitions
-trans = [
-    Transition(
-        np.ones(10) * i,
-        f"s{i}",
+if _TEST_SIGNLE_TRANSFER:
+    trans = [
+        Transition(
+            np.ones(10) * i,
+            f"s{i}",
+            0.7,
+            0.9,
+            np.ones(2),
+        )
+        for i in range(2)
+    ]
+
+    trans_bsize = len(pickle.dumps(trans[0]))
+    trans_window = MPI.Win.Allocate(trans_bsize * 2, comm=comm)
+
+else:
+    trans = Transition(
+        np.random.normal(0.0, 0.5, size=(10,)),
+        np.random.normal(0.0, 0.5, size=(2,)),
         0.7,
         0.9,
-        np.ones(2),
+        np.random.normal(0.0, 0.5, size=(2,)),
     )
-    for i in range(2)
-]
+    trans_bsize = len(pickle.dumps(trans))
+    trans_window = MPI.Win.Allocate(trans_bsize * 500, comm=comm)
 
-trans_bsize = len(pickle.dumps(trans[0]))
-trans_window = MPI.Win.Allocate(trans_bsize * 2, comm=comm)
-
-counter_window = MPI.Win.Allocate(4, comm=comm)
 
 if rank != 0:
     if _TEST_SIGNLE_TRANSFER:
@@ -350,9 +466,17 @@ if rank != 0:
         counter_window.Unlock(rank=0)
 
     else:
-        pass
-    # worker = Worker(comm, 100, _BUFFER_GTHR_STATE, _POLICY_BCST_STATE)
-    # worker.run()
+        worker = Worker(
+            comm,
+            net,
+            model_window,
+            trans_bsize,
+            trans_window,
+            counter_window,
+            total_replay_size=1100,
+            total_time_steps=5000,
+        )
+        worker.run()
 
 if rank == 0:
     if _TEST_SIGNLE_TRANSFER:
@@ -364,13 +488,24 @@ if rank == 0:
 
         # Read model state from the Learner's window
         counter_window.Lock(rank=0)
+        buff = bytearray(4)
+        counter_window.Get(buff, target_rank=0)
+        print(int.from_bytes(buff, byteorder="big"))
         print(int.from_bytes(counter_window, byteorder="big"))
         counter_window.Unlock(rank=0)
 
     else:
-        pass
-    # learner = Learner(comm, 10000, _BUFFER_GTHR_STATE, _POLICY_BCST_STATE)
-    # learner.learn()
+        learner = Learner(
+            comm,
+            net,
+            model_window,
+            trans_bsize,
+            trans_window,
+            counter_window,
+            total_replay_size=1100,
+            total_time_steps=5000,
+        )
+        learner.learn()
 
 
 # %%
@@ -392,3 +527,66 @@ if rank == 0:
 # win_array[:2] = policy[:2]
 # window.Flush(rank=1)
 # window.Sync()
+
+
+# Check if previous send is completed
+# if not MPI.Request.Test(self.req):
+# # If nto wait for completion
+# print(
+#     f"worker: {self.comm.Get_rank()}/{self.comm.Get_size()}, "
+#     f"previous send was not completed"
+# )
+# self.req.wait()
+
+# # Send rollouts to learner (non-blocking)
+# # self.req = self.comm.isend(self.buffer, dest=0, tag=11)
+# self.comm.gather(self.buffer, root=0)
+
+# # Get the updated policy
+# self.model = self.comm.bcast(self.model, root=0)
+
+
+# # Initialize empty request for sending rollouts
+# self.req = MPI.Request()
+
+
+# Create persistant receive communication
+# # self.req = self.communicator.Recv_init(source=1, tag=11)
+
+# # Send initialized policy to all workers
+# self.policy["W"] = 0.0
+# self.comm.bcast(self.policy, root=0)
+
+
+# if self.t == self.step_n:
+#     print(
+#         f"learner: training iteration: {i}, t:{time.time() - start_time}, "
+#         f"data length: {len(self.buffer)}"
+#     )
+
+#     # Received data from workers (non-blocking)
+#     # reqs = [
+#     #     self.comm.irecv(source=w, tag=11)
+#     #     for w in range(1, self.comm.Get_size())
+#     # ]
+
+#     # Gather the data from workers (blocking)
+#     data = self.comm.gather(self.buffer, root=0)
+
+#     # Send updated policy to all workers (blocking)
+#     self.comm.bcast(self.policy, root=0)
+
+#     # Wait untill all data is received
+#     # data = MPI.Request.waitall(reqs)
+
+#     # Postptocess the data
+#     print(
+#         f"learner: received data type: {type(data)}, len: {len(data)}, "
+#         f"my buffer len: {len(data[0])}"
+#     )
+#     self.buffer.append(data)
+#     self.buffer = self.buffer[-self.size :]
+
+# # Time deelay for training
+# time.sleep(1)
+# self.policy["W"] += 1.0
